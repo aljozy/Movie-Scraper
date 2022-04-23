094c8a21371996b29496368c46f564581bb028ff
Aljozy <aljozy2@yahoo.com>
2022-04-23 15:06:28 +0530

    add command line support

--- run.py
+++ run.py
@@ -1,4 +1,20 @@
 from Torrent.Torrent import Torrent
 
-with Torrent() as bot:
-    bot.muvi()
+try:
+
+    with Torrent() as bot:
+        bot.muvi()
+except Exception as e:
+    if 'in PATH' in str(e):
+        print(
+            'You are trying to run the bot from terminal \n'
+            'Please add to PATH your Selenium Drivers \n'
+            'Windows: \n'
+            '     set PATH=%PATH%;C:path-to-your-folder \n \n'
+            'Linux: \n'
+            '     PATH=$PATH:/path/toyour/folder/ \n'
+        )
+
+    else:
+        # for other exceptions
+        raise
